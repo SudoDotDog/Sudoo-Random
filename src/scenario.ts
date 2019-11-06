56c7f385554618a938f01f90935d6c8ceb3a401f
WMXPY <wm@mengw.io>
2019-11-06 11:17:57 -0600

    add longest scnerio version

--- src/scenario.ts
+++ src/scenario.ts
@@ -6,7 +6,13 @@
 
 import { randomPresent, randomString } from "./string";
 
-export const randomApiKey = (): string => {
+export const randomApiKey = (tails: number = 1): string => {
 
-    return `${randomPresent()}-${randomString(5)}-${randomString()}`;
+    if (tails <= 0) {
+        return `${randomPresent()}-${randomString()}`;
+    }
+
+    const actualTails: number = tails >= 10 ? 10 : tails;
+    const tailKeys: string[] = new Array(actualTails).fill(0).map(() => randomString());
+    return `${randomPresent()}-${randomString(5)}-${tailKeys.join('-')}`;
 };
---
+++ test/unit/scenario.test.ts
@@ -0,0 +1,42 @@
+/**
+ * @author WMXPY
+ * @namespace Random
+ * @description Scenario
+ * @override Unit
+ */
+
+import { expect } from 'chai';
+import * as Chance from 'chance';
+import { randomApiKey } from '../../src';
+
+describe('Given [Scenario] Functions', (): void => {
+
+    const chance: Chance.Chance = new Chance('random-scenario');
+
+    it('should be able to create random api key', (): void => {
+
+        const value: string = randomApiKey();
+
+        expect(value).to.be.lengthOf(8 + 5 + 10 + 2);
+    });
+
+    it('should be able to create no-tail key', (): void => {
+
+        const value1: string = randomApiKey(0);
+        const value2: string = randomApiKey(-15);
+
+        expect(value1).to.be.lengthOf(8 + 10 + 1);
+        expect(value2).to.be.lengthOf(8 + 10 + 1);
+    });
+
+    it('should be able to create long-tail key', (): void => {
+
+        const value1: string = randomApiKey(5);
+        const value2: string = randomApiKey(10);
+        const value3: string = randomApiKey(20);
+
+        expect(value1).to.be.lengthOf(8 + 5 + (10 * 5) + (2 + 5 - 1));
+        expect(value2).to.be.lengthOf(8 + 5 + (10 * 10) + (2 + 10 - 1));
+        expect(value3).to.be.lengthOf(8 + 5 + (10 * 10) + (2 + 10 - 1));
+    });
+});
