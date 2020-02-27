f8870fbedbf159292d43d3112a70a4c376ef7ba7
PCXPY <pc@wmpcxpy.com>
2020-02-27 11:14:26 -0600

    [feat] fix input methods

--- src/array.ts
+++ src/array.ts
@@ -4,13 +4,13 @@
  * @description Array
  */
 
-import { randomIntegerBelow } from "./number";
+import { randomNaturalBelow } from "./number";
 
 export const randomElement = <T extends any = any>(array: T[]): T => {
 
     if (array.length === 0) {
         return undefined as any as T;
     }
-    const index: number = randomIntegerBelow(array.length);
+    const index: number = randomNaturalBelow(array.length);
     return array[index] as T;
 };
--- src/number.ts
+++ src/number.ts
@@ -4,12 +4,19 @@
  * @description Number
  */
 
-export const randomIntegerBelow = (max: number): number => {
+export const randomNaturalBelow = (max: number): number => {
+
     const maxInteger: number = Math.floor(max);
     return Math.floor(Math.random() * maxInteger);
 };
 
+export const randomIntegerBelow = (max: number): number => {
+
+    return randomNaturalBelow(max);
+};
+
 export const randomIntegerBetween = (min: number, max: number): number => {
+
     const minInteger = Math.ceil(min);
     const maxInteger = Math.floor(max);
     return Math.floor(Math.random() * (maxInteger - minInteger + 1) + minInteger);
